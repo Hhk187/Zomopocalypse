92c4356a0fffafec2deccddd6c13fd403c239de8
Hhk187 <2003hasnin@gmail.com>
2025-08-02 22:14:40 +0200

    added: - new apratement, not fully built yet

--- Scenes/Player/ledge_contact.gd
+++ Scenes/Player/ledge_contact.gd
@@ -10,7 +10,7 @@ func _physics_process(delta: float) -> void:
 	if (bottom_ray.is_colliding() and 
 		Input.is_action_pressed("play_forward") and 
 		owner.is_on_floor() and
-		bottom_ray.get_collision_normal().y == 0 and 
+		(bottom_ray.get_collision_normal().y == 0 or floor_ray.get_collision_normal().y == 1) and
 		not top_ray.is_colliding()
 		):
 			owner.global_position.y += abs(owner.global_position.y - floor_ray.get_collision_point().y)
