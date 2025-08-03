d2712dc0f51f6aa4221467e94549c17f1d8181d2
Hhk187 <2003hasnin@gmail.com>
2025-08-03 02:52:45 +0200

    added: - door - some of my genius

--- Scenes/BuildingsBlock/Components/Door/door.gd
+++ Scenes/BuildingsBlock/Components/Door/door.gd
@@ -12,10 +12,10 @@ func _on_interact():
 	if not is_operational(): return
 	match door_state:
 		DOOR_STATE_TYPE.OPEN:
-			rotation_degrees.y = 90
+			rotation_degrees.y = 0
 			door_state = DOOR_STATE_TYPE.CLOSE
 			_play_sound(load("res://Assets/SFX/WorldDecor/Openning/Open.wav"))
 		DOOR_STATE_TYPE.CLOSE:
-			rotation_degrees.y = 0
+			rotation_degrees.y = 90
 			door_state = DOOR_STATE_TYPE.OPEN
 			_play_sound(load("res://Assets/SFX/WorldDecor/Openning/Open.wav"))
--- Scenes/Player/ledge_contact.gd
+++ Scenes/Player/ledge_contact.gd
@@ -3,12 +3,17 @@ extends Node3D
 @onready var top_ray: RayCast3D = $TopRay
 @onready var floor_ray: RayCast3D = $TopRay/FloorRay
 @onready var bottom_ray: RayCast3D = $BottomRay
-
-
+var input_dir
+var cal
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	input_dir = Input.get_vector("play_right", "play_left", "play_backward", "play_forward")
+	
+	cal =  asin(input_dir.x)# + asin(input_dir.x)
+	rotation.y = cal
+	
 	if (bottom_ray.is_colliding() and 
-		Input.is_action_pressed("play_forward") and 
+		input_dir and 
 		owner.is_on_floor() and
 		(bottom_ray.get_collision_normal().y == 0 or floor_ray.get_collision_normal().y == 1) and
 		not top_ray.is_colliding()
